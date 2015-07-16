8df6ff2da163b2ec24b6c24c5a3c2e3410297dd6
Gustavo Cortes <gustavocortesl@gmail.com>
2015-07-17 01:02:34 +0200

    Lista de preguntas y autoload

--- models/models.js
+++ models/models.js
@@ -34,13 +34,21 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz;	//exportar definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
-  // success(...) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function(count) {
+sequelize.sync().then(function() {
+  // then(...) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function(count) {
     if (count === 0) { // la tabla se inicializa solo si está vacía
       Quiz.create({
 		    pregunta: 'Capital de Italia',
 		    respuesta: 'Roma'
+		  });
+      Quiz.create({
+		    pregunta: 'Capital de Portugal',
+		    respuesta: 'Lisboa'
+		  });
+      Quiz.create({
+		    pregunta: 'Capital de Francia',
+		    respuesta: 'París'
 		  })
 	  .success(function() {console.log('Base de datos inicializada')});
     }
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,20 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-/* GET question page. */
-router.get('/quizes/question', quizController.question);
-/* GET answer page. */
-router.get('/quizes/answer', quizController.answer);
-/* GET author page. */
+// Autoload de rutas con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definición de rutas de quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+// Otras rutas
 router.get('/author', quizController.author);
 
 module.exports = router;
