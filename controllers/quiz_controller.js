ca1055c0a3e8132854cf13a630e09a699507f7c2
Gustavo Cortes <gustavocortesl@gmail.com>
2015-07-20 00:59:09 +0200

    Búsqueda de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,11 +17,20 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes });
-    }
-  ).catch(function(error) { next(error); });
+  if (req.query.search == null) {
+    models.Quiz.findAll().then(
+      function(quizes) {
+	res.render('quizes/index', { quizes: quizes });
+      }
+    ).catch(function(error) { next(error); });
+  } else {
+    var search = "%" + req.query.search.replace(" ", "%") + "%";    
+    models.Quiz.findAll({where: ["pregunta like ?", search]}).then(
+      function(quizes) {
+	res.render('quizes/index', { quizes: quizes });
+      }
+    ).catch(function(error) { next(error); });
+  }
 };
 
 // GET /quizes/:id
