a196b4ae86b45a50fdfdf0a98e1c37e9eba2e77a
Gustavo Cortes <gustavocortesl@gmail.com>
2015-08-20 00:13:38 +0200

    Estadísticas con count correcto)

--- controllers/stats_controller.js
+++ controllers/stats_controller.js
@@ -7,91 +7,29 @@ var stats = {
 	quizesSinComments: 'ND',
 	quizesConComments: 'ND'
 };
-// Cargar modelo ORM
-var Sequelize = require('sequelize');
 
 // GET /quizes/statistics
 exports.statistics = function(req, res, next) {
-  models.Quiz.count().then(function(total) {
-		stats.totalQuizes = total;
-    console.log('quizes '+stats.totalQuizes+' '+total);
+  models.Quiz.count().then(function(totalQuizes) {
+    stats.totalQuizes = totalQuizes;
+    console.log('quizes '+stats.totalQuizes);
   }).then(
-  models.Comment.count().then(function(total) {
-		stats.totalComments = total;
-		console.log('comments '+stats.totalComments+' '+total);
+  models.Comment.count().then(function(totalComments) {
+    stats.totalComments = totalComments;
+    console.log('comments '+stats.totalComments);
   }).then(
-    models.Quiz.findAll({
-      include: [{
-	model: models.Comment//,
-	//where: { QuizId: Sequelize.col('quiz.id') }
-      }]
-    }).then(function(quizes) {
-	//console.log(JSON.stringify(quizes));
-	var count = 0;
-	quizes.forEach(function (q) {
-	  console.log('> ', q.pregunta, q.Comments.length);
-	  if (q.Comments.length) count++;  
-	})
-	stats.quizesConComments = count;
-  	console.log('quizesConComments '+stats.quizesConComments);
-    /*models.Quiz.findAll({
-    include: [{
-      model: models.Comment,
-      where: { QuizId: Sequelize.col('quiz.id') }
-    }]
-  }).then(function(total) {
-	console.log(JSON.stringify(total));
-	stats.quizesConComments = total.length;
-  	console.log('quizesConComments '+stats.quizesConComments);*/
-  }).then(
-    function(error) {
-      // Cálculo de estadísticas restantes
-      stats.mediaComments = (stats.totalComments / stats.totalQuizes).toFixed(2);
-      stats.quizesSinComments = stats.totalQuizes - stats.quizesConComments;
-    // renderización de la página
-    res.render('quizes/statistics', { stats: stats, errors: [] });
-  })));
-/*// obtenemos estadísticas en modo asíncrono
-  async.parallel([
-
-    function(callback) { //número de quizes
-  	models.Quiz.count().then(
-  	  function(total) {
-  	    stats.totalQuizes = total;
-  	    console.log('quizes '+stats.totalQuizes+' '+total);
-  	  }
-        ).catch(function(error) { callback(error); return; });
-        callback();        
-    },
-    function(callback) { //número de comentarios
-  		models.Comment.count().then(
-  	  function(total) {
-  	    stats.totalComments = total;
-  	    console.log('comments '+stats.totalComments+' '+total);
-  	  }
-        ).catch(function(error) { callback(error); return; });
-        callback();        
-    },
-    function(callback) { //número de preguntas con comentarios
-  		models.Quiz.findAll({
-				include: [{
-	  			model: models.Comment,
-	    		        where: { QuizId: Sequelize.col('quiz.id') }
-  			}]
-			}).then(
-  	  	function(total) {
-					console.log(JSON.stringify(total));
-		 	    stats.quizesConComments = total.length;
-  		    console.log('quizesConComments '+stats.quizesConComments);
-  		  }).catch(function(error) { callback(error); return ;});
-        callback();        
-    }
-  ],
-  function(error) {
+  models.Quiz.count({ 
+		      include: [{ model: models.Comment, required: true }],
+		      distinct: true
+		    }).then(function(quizesConComments) {
+    stats.quizesConComments = quizesConComments;
+    console.log('quizesConComments '+stats.quizesConComments);
     // Cálculo de estadísticas restantes
     stats.mediaComments = (stats.totalComments / stats.totalQuizes).toFixed(2);
+    console.log('mediaComments '+stats.mediaComments);
     stats.quizesSinComments = stats.totalQuizes - stats.quizesConComments;
+    console.log('quizesSinComments '+stats.quizesSinComments);
     // renderización de la página
     res.render('quizes/statistics', { stats: stats, errors: [] });
-  });*/
+  }))).catch(function(error) { next(error); });
 };
